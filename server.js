1b1e1694d2178b4511c2627a138fa09f74aa1682
Glenn Kirchner <greywolf359@gmail.com>
2017-05-09 20:41:00 -0700

    add todos todos id

--- server.js
+++ server.js
@@ -2,8 +2,51 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
+var todos = [
+	{
+		id: 1,
+		description: "meet for lunch",
+		completed: false
+	},
+	{
+		id: 2,
+		description: "go shopping",
+		completed: false
+	},
+	{
+		id: 3,
+		description: "feed dog",
+		completed: true
+	}
+];
+
 app.get('/', (req, res)=>{
 	res.send('todo api');
+})
+
+app.get('/todos', (req,res)=>{
+	res.json(todos);
+})
+
+app.get('/todos/:id', (req,res)=>{
+	var id = parseInt(req.params.id);
+	console.log('id: ', id, typeof id)
+	//iterate over array and find the id
+	var todo = todos.find((element)=>{
+		console.log(element.id);
+			return element.id === id
+			
+		}
+	)
+	console.log(todo);
+	if(todo){
+		//res.status(200).send(todo);
+		res.status(200).json(todo);
+	}else{
+		res.status(404).send('Todo Not found');
+	}
+
+	//if not found send a 404
 })
 
 app.listen(PORT,()=>{
