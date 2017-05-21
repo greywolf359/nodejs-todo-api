b87979f8b226e26668e25ae03c4884858dd335cf
Glenn Kirchner <greywolf359@gmail.com>
2017-05-20 20:36:51 -0700

    add force option to sync

--- server.js
+++ server.js
@@ -248,7 +248,7 @@ app.post('/users/login',(req,res)=>{
 	//*************************************************************************
 })
 
-db.sequelize.sync(/*{force: true}*/).then(()=>{
+db.sequelize.sync({force: true}).then(()=>{
 	app.listen(PORT,()=>{
 		console.log('Listening on port: ', PORT);
 })
